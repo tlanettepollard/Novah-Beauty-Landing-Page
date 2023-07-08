5900fee4bdf40b46cd74d71013c4b9efc33026e9
T. Lanette <TLPollard4516@gmail.com>
2023-07-08 15:47:27 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,36 +14,11 @@ carousel.addEventListener('slide.bs.carousel', (event) => {
 		.classList.add('active');
 });
 
-// Testimonial Slide
+// Gallery Slide
 
 const swiperEl = document.querySelector('swiper-container');
 
 const params = {
-	injectStyles: [
-		`
-		.swiper-pagination-bullet {
-			width: 20px;
-			height: 20px;
-			text-align: center;
-			line-height: 20px;
-			font-size: 12px;
-			color: #000;
-			opacity: 1;
-			background: rgba(0, 0, 0, 0.2);
-		}
-
-		.swiper-pagination-bullet-active {
-			color: #fff;
-			background: #007aff;
-		}
-	`,
-	],
-	pagination: {
-		clickable: true,
-		renderBullet: function (index, className) {
-			return '<span class="' + className + '">' + (index + 1) + '</span>';
-		},
-	},
 	slidesPerView: 1,
 	breakpoints: {
 		640: {
